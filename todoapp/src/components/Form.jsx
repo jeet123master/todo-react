484267bd66ad773167a81a7adbb4851728a1145f
jeet123master <jeetlimboo21@gmail.com>
2023-10-31 20:01:18 +0300

    adding todo item

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -1,9 +1,32 @@
+import { useLayoutEffect, useState } from 'react';
 import Styles from './form.module.css';
 export default function Form(){
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    function handleSubmit(e){
+        e.preventDefault();
+        setTodos([...todos, todo])
+        setTodo("")
+    }
     return (
-        <form className={Styles.form}>
-            <input type="text" />
-            <button>Add</button>
-        </form>
+        <div>
+            <form className={Styles.form} onSubmit={handleSubmit}>
+                <input 
+                    className={Styles.input} 
+                    type="text"
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                />
+                <button
+                    type='submit'
+                    className={Styles.button}
+                >Add</button>
+            </form>
+            {
+                todos.map((item)=> 
+                <ul key={item}>{item}</ul>)
+            }
+        </div>
+        
     )
 }
